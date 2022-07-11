ef1c7520fd95383529c46c30da35fda3593fa2e9
Hexton Ho <hextonho@icloud.com>
2022-07-12 02:37:57 +0800

    Convert app class to func component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import { Component } from 'react';
-
+import { useEffect, useState } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    console.log('Effect fited');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        setMonsters(users);
+      });
+  }, []);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  });
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monster Rolodex</h1>
+
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={'Search Monster'}
+        className={'search-box'}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+/* 
 class App extends Component {
   constructor() {
     super();
@@ -50,6 +86,7 @@ class App extends Component {
       </div>
     );
   }
-}
+} 
+*/
 
 export default App;
