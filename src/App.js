1c14d8df007730600ea900380d31ef788fec4c09
Hexton Ho <hextonho@icloud.com>
2022-07-14 01:30:51 +0800

    Change tab name

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 const App = () => {
+  console.log('Render');
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
 
